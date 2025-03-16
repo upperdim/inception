8c0de6a63e3758b27e39a61d600331e6660a1bbe
upperdim <45048352+upperdim@users.noreply.github.com>
2025-03-16 20:30:45 +0100

    check existing config

--- srcs/requirements/wordpress/install_wordpress.sh
+++ srcs/requirements/wordpress/install_wordpress.sh
@@ -25,18 +25,17 @@ until mysqladmin ping -h mariadb -uroot -ppass --silent; do
     sleep 2
 done
 
-#echo "Waiting for MariaDB to be ready..."
-#until mysqladmin ping -h "${MYSQL_DB_HOST}" -u root --password="${MYSQL_ROOT_PASSWORD}" --silent; do
-#    sleep 1
-#done
 
-
-echo "Wordpress: Creating config..."
-./wp-cli.phar config create	--dbname="$MARIADB_DB" \
-							--dbuser="$MARIADB_USER" \
-							--dbpass="$MARIADB_PW" \
-							--dbhost="mariadb" \
-							--allow-root
+if [ ! -f wp-config.php ]; then
+	echo "Wordpress: Creating config..."
+	./wp-cli.phar config create	--dbname="$MARIADB_DB" \
+															--dbuser="$MARIADB_USER" \
+															--dbpass="$MARIADB_PW" \
+															--dbhost="mariadb" \
+															--allow-root
+else
+	echo "Wordpress: existing config found"
+fi
 
 
 echo "Wordpress: Installing..."
